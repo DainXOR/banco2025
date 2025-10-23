9501cb549cbb8644c04ee01d2b974ee7c51f2ddf
Daniel León <65270757+DainXOR@users.noreply.github.com>
2025-10-23 18:02:12 -0500

    Added clients controller tests

--- src/test/java/com/udea/dainxor/banco2025/Banco2025ApplicationTests.java
+++ src/test/java/com/udea/dainxor/banco2025/Banco2025ApplicationTests.java
@@ -1,19 +1,31 @@
 package com.udea.dainxor.banco2025;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.udea.dainxor.banco2025.controller.CustomerController;
 import com.udea.dainxor.banco2025.controller.FakerController;
+import com.udea.dainxor.banco2025.dto.CustomerDTO;
+import com.udea.dainxor.banco2025.dto.DepositDTO;
+import com.udea.dainxor.banco2025.dto.TransactionDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class Banco2025ApplicationTests {
 
 	@Autowired
 	FakerController fakerController;
+	@Autowired
+	CustomerController customerController;
+
+	private CustomerDTO testCustomer;
+	private TransactionDTO testTransaction;
 
 	@Test
 	void health(){
@@ -25,6 +37,7 @@ class Banco2025ApplicationTests {
 		assertEquals("VERSION IS 1.0.0", fakerController.version());
 	}
 
+	// > Faker tests
 	@Test
 	void nationLength(){
 		Integer nationsLength = fakerController.getRandomNations().size();
@@ -65,6 +78,73 @@ class Banco2025ApplicationTests {
 		Integer aviationsLength = fakerController.getRandomAviation().size();
 		assertEquals(20, aviationsLength);
 	}
+
+	// > End Faker tests
+	// > Customer tests
+	@Test
+	void createCustomer() {
+		testCustomer = new CustomerDTO();
+		testCustomer.setFirstName("Test");
+		testCustomer.setLastName("User");
+		testCustomer.setAccountNumber("1234567890");
+		testCustomer.setBalance(1000.0);
+		testCustomer.setId(123123123L);
+		CustomerDTO createdCustomer = customerController.createCustomer(testCustomer).getBody();
+
+		assertNotNull(createdCustomer);
+		assertNotNull(createdCustomer.getId());
+		assertNotEquals(123123123L, createdCustomer.getId());
+		assertNotNull(createdCustomer.getAccountNumber());
+		assertNotEquals("1234567890", createdCustomer.getAccountNumber());
+		assertEquals("Test", createdCustomer.getFirstName());
+		assertEquals("User", createdCustomer.getLastName());
+		assertEquals(0.0, createdCustomer.getBalance());
+	}
+
+	@Test
+	void getAllCustomersNotNull(){
+		List<CustomerDTO> customers = customerController.getAll().getBody();
+        assertNotNull(customers);
+        assertFalse(customers.isEmpty());
+	}
+
+	@Test
+	void getByIdNotFound(){
+		var response = customerController.getById(9999L);
+		assertTrue(response.getStatusCode().is4xxClientError());
+	}
+
+	@Test
+	void getById(){
+		var response = customerController.getById(testCustomer.getId());
+		assertEquals(testCustomer, response.getBody());
+	}
+
+	@Test
+	void getByAccountNumber(){
+		var response = customerController.getByAccountNumber(testCustomer.getAccountNumber());
+		assertEquals(testCustomer, response.getBody());
+	}
+
+	@Test
+	void depositMoney(){
+		Double depositAmount = 500.0;
+		var depositDTO = new DepositDTO();
+		depositDTO.setId(testCustomer.getId());
+		depositDTO.setAmount(depositAmount);
+
+		var response = customerController.depositMoney(depositDTO);
+		assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+		CustomerDTO updatedCustomer = response.getBody();
+		assertNotNull(updatedCustomer);
+		assertEquals(testCustomer.getBalance() + depositAmount, updatedCustomer.getBalance());
+		testCustomer = updatedCustomer;
+	}
+
+	// > End Customer tests
+	// > Transaction tests
+
+	// > End Transaction tests
 }
 
 
